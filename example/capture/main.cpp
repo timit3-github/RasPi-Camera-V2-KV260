ab835b5b6ee8bb590f0b57b23fd332c8b00b5fa5
timit <battleonthebridge@gmail.com>
2024-06-01 23:45:50 +0900

    Add OpenCV example

---
+++ example/capture/main.cpp
@@ -0,0 +1,36 @@
+﻿//#include <opencv2/opencv.hpp>
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/videoio.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+#define WINDOWNAME "imx708"
+//#define ENABLE_FULLSCREEN
+
+int main(int argc, char** argv)
+{
+
+    cv::VideoCapture cap("v4l2src device=/dev/video0 ! video/x-raw, width=1536, height=864 ! appsink sync=false", cv::CAP_GSTREAMER);
+    cv::Mat frame;
+
+#ifdef ENABLE_FULLSCREEN
+    cv::namedWindow(WINDOWNAME, cv::WINDOW_NORMAL | cv::WINDOW_KEEPRATIO | cv::WINDOW_GUI_NORMAL);
+    cv::setWindowProperty(WINDOWNAME, cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
+#else
+    cv::namedWindow(WINDOWNAME, cv::WINDOW_AUTOSIZE | cv::WINDOW_KEEPRATIO | cv::WINDOW_GUI_NORMAL);
+#endif
+
+    while (1) {
+        cap >> frame;
+        cv::imshow(WINDOWNAME, frame);
+        auto key = cv::waitKey(1);
+        if (key == 'q' || key == 27)
+            break;
+    }
+	
+    return 0;
+}
